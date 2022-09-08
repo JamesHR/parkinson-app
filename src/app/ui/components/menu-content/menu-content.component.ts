527804ac08673010a6ffd6570d92b32478ae30a5
James HR <43450921+JamesHR@users.noreply.github.com>
2022-09-08 02:06:25 -0500

    feat: patients selector before test

--- src/app/ui/components/menu-content/menu-content.component.ts
+++ src/app/ui/components/menu-content/menu-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-content',
@@ -8,6 +9,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MenuContentComponent implements OnInit {
 
   @Input() activeOption: string;
+  isModalOpen = false;
+  searchValue = '';
 
   menu = [
     { icon: 'home', value: 'Inicio', isActive: false, path: '/dashboard-home'},
@@ -15,7 +18,20 @@ export class MenuContentComponent implements OnInit {
     { icon: 'settings', value: 'Configuraciones', isActive: false, path: '/dashboard-settings'}
   ];
 
-  constructor() { }
+  patients = [
+    'Claudia Ortiz Perez',
+    'Ricardo Flores Vazquez',
+    'José Carlos Ramírez Magón',
+    'Laura Morales Flores',
+    'Maria Gonzalez Reyes',
+    'Juan Fernandez Fernandez',
+  ];
+
+  filterList: Array<string> = this.patients;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.ionViewWillEnter();
@@ -23,5 +39,17 @@ export class MenuContentComponent implements OnInit {
 
   ionViewWillEnter = () =>
     this.menu.forEach(item => item.isActive = this.activeOption === item.value);
+
+  setOpenModal = (isOpen: boolean = true) => this.isModalOpen = isOpen;
+
+  searchCustomer(value: string){
+    this.searchValue = value;
+    this.filterList = this.patients.filter(element => element.toUpperCase().includes(value.toUpperCase()));
+  }
+
+  openCatalog() {
+    this.setOpenModal(false);
+    setTimeout(() =>this.router.navigate(['/test-catalog']), 100);
+  }
 
 }
